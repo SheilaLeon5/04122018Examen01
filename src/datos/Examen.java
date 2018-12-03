9b532b50029ee38b3ac42dda0abb87b24892bb01
JOSE MAURICIO SANCHEZ RUBIO <jmaurici@idecnet.com>
2018-12-03 21:01:02 +0000

    Versión inicial 4 de diciembre de 2018

---
+++ src/datos/Examen.java
@@ -0,0 +1,9 @@
+package datos;
+
+public class Examen {
+	// Objetivos 1 y 2
+
+	private int[][] notasAlumnos = { { 5, 6, 3, 9, 8 }, { 7, 2, 8, 4, 5 }, { 8, 5, 6, 2, 4 } };
+	private String[] alumnos = { "Pepe", "Manuel", "Carlos" };
+	private String[] modulos = { "PRO", "SSF", "LND", "LNT", "FOL" };
+}
