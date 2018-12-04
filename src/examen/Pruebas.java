d8b8e041b0a0b88905c45ed9b1540419e2ccf2fb
SheilaLeon <SheilaLeon@T20211>
2018-12-04 08:58:37 +0000

    Objetivo 1,2,3 REALIZADOS

--- src/examen/Pruebas.java
+++ src/examen/Pruebas.java
@@ -6,7 +6,21 @@ public class Pruebas {
 
 	// Desarrolllar los métodos del examen AQUI (ver Objetivos)
 	
+	//Objetivo 2.
 	private Examen[] objetos = new Examen[5];
+	
+	
+	// Objetivo 3.
+	public int productoDosNumeros (int a, int b) {
+		int resultado = 0;
+		for (int i = 0; i < b; i++) {
+			resultado =+ a + resultado; 
+		}
+		//System.out.println(resultado);
+		return resultado;
+		
+	}
+	
 	
 	
 
@@ -14,8 +28,17 @@ public class Pruebas {
 		// Hacer las llamadas a los métodos AQUI.
 		// (no olvidar recoger los valores devueltos
 		
-		
 		Pruebas ejercicios= new Pruebas();
+
+		
+		//Objetivo 1.
+		int a = 4;
+		int b = 7;
+		ejercicios.productoDosNumeros(a,b);
+		
+	
+		
+		//Objetivo 2.
 		Examen objeto1 = new Examen(7,"Matematicas",'M',false);
 		Examen objeto2 = new Examen(4,"Lengua",'F',true);
 		Examen [] resultado = {objeto1 , objeto2};
