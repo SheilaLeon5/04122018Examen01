6edeb24ebd423468db11c6f7cf562cac763b7880
sheila <sheila@Len>
2018-12-07 14:02:28 +0000

    Examen 1.- 04Diciembre2018 - 1ºTrimestre (CORREGIDO)

--- src/datos/Examen.java
+++ src/datos/Examen.java
@@ -57,7 +57,12 @@ public class Examen {
 
 
 
-	private int[][] notasAlumnos = { { 5, 6, 3, 9, 8 }, { 7, 2, 8, 4, 5 }, { 8, 5, 6, 2, 4 } };
+	private int[][] notasAlumnos = { 
+			{ 5, 6, 3, 9, 8 }, 
+			{ 7, 2, 8, 4, 5 }, 
+			{ 8, 5, 6, 2, 4 } 
+	};
+					
 	private String[] alumnos = { "Pepe", "Manuel", "Carlos" };
 	private String[] modulos = { "PRO", "SSF", "LND", "LNT", "FOL" };
 
--- src/examen/Pruebas.java
+++ src/examen/Pruebas.java
@@ -9,7 +9,23 @@ public class Pruebas {
 	// Desarrolllar los métodos del examen AQUI (ver Objetivos)
 	
 	//Objetivo 2.
-	private Examen[] objetos = new Examen[5];
+	private Examen[] objetos = new Examen[5];   
+	
+	
+	/*     --MEJORA OBJETIVO 2.2--
+	 * Objetivo 2.2 añadir los dos objetos anteriores a un array de esa clase,
+	 *  de 5 elementos de tamaño.
+	 *  
+	 * --- >    Examen[] miArray = {objeto1,objeto2,null,null,null}
+	 * 
+	 *   Yo hice: 
+	 *    private Examen[] objetos = new Examen[5]; 
+	 *    Examen objeto1 = new Examen(7,"Matematicas",'M',false);
+	 *	  Examen objeto2 = new Examen(4,"Lengua",'F',true);
+	 *	  Examen [] resultado = {objeto1 , objeto2};
+	 *	  ejercicios.objetos = resultado;
+	 * 
+	 */
 
 
 	//Objetivo 4.  
@@ -19,7 +35,7 @@ public class Pruebas {
 			if(n > objetos.length) {
 				int i = 0;
 				while(i < objetos.length) {
-					resultado[i] = objetos[i];
+					resultado[i] = objetos[i]; // Otra posibilidad: Quitar el incremento de i y ponerlo en 'resultado[i++]'
 					i++;
 				}	
 			}else {
@@ -40,7 +56,7 @@ public class Pruebas {
 	public int productoDosNumeros (int a, int b) {
 		int resultado = 0;
 		for (int i = 0; i < b; i++) {
-			resultado =+ a + resultado; 
+			resultado =+ a + resultado; // Mejor sin la variable resultado. Es decir --> resultado =+ a;
 		}
 		//System.out.println(resultado);
 		return resultado;
@@ -50,11 +66,38 @@ public class Pruebas {
 	//Objetivo 5.
 	private Examen[] alumnos;
 	public void notaMediasAlumnos() {
-		for (int i = 0; i < alumnos.length; i++) {
+		
+		Examen examen = new Examen();
+		int[][] notas = examen.getNotasAlumnos();
+		String[] alumnos = examen.getAlumnos();
+		int numModulos = examen.getModulos().length;
 
-			System.out.println(alumnos[i].getAlumnos() + " -->  nota media : "+ alumnos[i].getNotasAlumnos());
+		for (int i = 0; i < alumnos.length; i++) {
+			int acumulado = 0;
+			for (int j = i; j < notas[0].length; j++) { // número de la primera fila 
+				acumulado += notas[i][j];
+			}
+			//System.out.println("Nota media de " + alumnos[i] + " --> " + acumulado/numModulos);
 		}
 	}
+	
+	
+	
+	//Objetivo 6.
+		public void notaMediaModulos() {
+			Examen examen = new Examen();
+			int[][] notas = examen.getNotasAlumnos();
+			String[] modulos = examen.getModulos();
+
+			for (int j = 0; j < notas[0].length; j++) {
+				int acumulado = 0;
+				for (int i = 0; i < notas.length; i++) { // número de la primera fila 
+					acumulado += notas[i][j];
+				}
+				System.out.println("Nota media de " + modulos[j] + " es: " + acumulado / notas.length );
+			}
+		}
+	
 
 
 	public static void main(String[] args) {
@@ -64,23 +107,25 @@ public class Pruebas {
 		Pruebas ejercicios= new Pruebas();
 
 		//Objetivo 2.
+
 		Examen objeto1 = new Examen(7,"Matematicas",'M',false);
 		Examen objeto2 = new Examen(4,"Lengua",'F',true);
 		Examen [] resultado = {objeto1 , objeto2};
 		ejercicios.objetos = resultado;
 		
 		//Objetivo 3.
-		int a = 4;
-		int b = 7;
+		int a = 6;
+		int b = 4;
 		ejercicios.productoDosNumeros(a,b);
 		
 		//Objetivo 4.
-		ejercicios.creaObjetosExamen(4);
+		 Examen [] examenes = ejercicios.creaObjetosExamen(4);  // mejor recoger resultados. Es decir --> Examen[] examenes
 		
 		//Objetivo 5.
-		Examen [] alumnos = {};		
+		ejercicios.notaMediasAlumnos();	
 		
-		
+		//Objetivo 6.
+		ejercicios.notaMediaModulos();
 	
 		
 	}
